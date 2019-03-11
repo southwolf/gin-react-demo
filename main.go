7194ad017d623505f5f7175d3370e391703f6156
Rick Song <risong@xogrp.com>
2019-03-11 10:05:55 +0800

    Static page & basic apis

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
+
+// Joke contains information about a single Joke
+type Joke struct {
+	ID    int    `json:"id" binding:"required"`
+	Likes int    `json:"likes"`
+	Joke  string `json:"joke" binding:"required"`
+}
+
+var jokes = []Joke{
+	Joke{1, 0, "Did you hear about the restaurant on the moon? Great food, no atmosphere."},
+	Joke{2, 0, "What do you call a fake noodle? An Impasta."},
+	Joke{3, 0, "How many apples grow on a tree? All of them."},
+	Joke{4, 0, "Want to hear a joke about paper? Nevermind it's tearable."},
+	Joke{5, 0, "I just watched a program about beavers. It was the best dam program I've ever seen."},
+	Joke{6, 0, "Why did the coffee file a police report? It got mugged."},
+	Joke{7, 0, "How does a penguin build it's house? Igloos it together."},
+}
 
 func main() {
 	router := gin.Default()
@@ -19,7 +37,32 @@ func main() {
 				"message": "pong",
 			})
 		})
+
+		api.GET("/jokes", JokeHandler)
+		api.POST("/jokes/like/:jokeID", LikeJoke)
 	}
 
 	router.Run(":3000")
+}
+
+// JokeHandler retrieves a list of jokes
+func JokeHandler(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, jokes)
+}
+
+// LikeJoke increments the likes of a particular joke item
+func LikeJoke(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	if jokeID, err := strconv.Atoi(c.Param("jokeID")); err == nil {
+		for i := 0; i < len(jokes); i++ {
+			if jokes[i].ID == jokeID {
+				jokes[i].Likes++
+			}
+		}
+		// return a pointer to the updated jokes list
+		c.JSON(http.StatusOK, &jokes)
+	} else {
+		c.AbortWithStatus(http.StatusNotFound)
+	}
 }
